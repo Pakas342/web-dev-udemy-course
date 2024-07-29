52a281d1a9d34ff131a9234e0ff983c471bfc1ef
Juan Barrera <jcbp1999@gmail.com>
2024-07-29 17:10:10 +1000

    random buyer test

---
+++ js playground/lunchBuyer.js
@@ -0,0 +1,16 @@
+
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        
+        var payer = (Math.random() * names.length);
+        payer = Math.floor(payer);
+        return names[payer] + ' is going to buy lunch today!'
+        
+        
+    
+    
+    /******Don't change the code below*******/    
+    }
