de74f3f88ad7401a753dcd23cdab6bf9ca4ef562
Juan Barrera <jcbp1999@gmail.com>
2024-07-30 23:20:38 +1000

    js selectors

--- 12 DOM play/main.js
+++ 12 DOM play/main.js
@@ -1,6 +1,6 @@
 // code for chaning the color of a specific element:
-// document.querySelector("ul li a").style.color = "red":
+document.querySelector("ul li a").style.color = "red":
 
 // code for changing attributes of specific element:
-// document.querySelector("a").getAttribute("href");
-// document.querySelector("a").setAttribute("href", "https://www.wikipedia.com");
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://www.wikipedia.com");
