a909f7154f037e6fc5b5126d4f9dcb41bf338339
Juan Barrera <jcbp1999@gmail.com>
2024-08-13 18:48:32 +1000

    final update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -12,50 +12,6 @@ function reproduceAudio() {
     sound.play()
 }
 
-function reproduceAudioFromKey(event) {
-var buttonInnetHtml = event.key;
-
-    switch (buttonInnetHtml) {
-        case "w" :
-            var crash = new Audio("./sounds/crash.mp3");
-            crash.play();
-            break;
-        
-        case "a" :
-            var crash = new Audio("./sounds/kick.mp3");
-            crash.play();
-            break;
-        
-        case "s" :
-            var crash = new Audio("./sounds/snare.mp3");
-            crash.play();
-            break;  
-
-        case "d" :
-            var crash = new Audio("./sounds/tom1.mp3");
-            crash.play();
-            break;  
-
-        case "j" :
-            var crash = new Audio("./sounds/tom2.mp3");
-            crash.play();
-            break;  
-            
-        case "k" :
-            var crash = new Audio("./sounds/tom3.mp3");
-            crash.play();
-            break;  
-
-        case "l" :
-            var crash = new Audio("./sounds/tom4.mp3");
-            crash.play();
-            break;  
-        
-        default: console.log(this)
-    }
-}
-
-
 document.addEventListener('keydown', reproduceAudioFromKey)
 
 function reproduceAudioFromKey(event) {
